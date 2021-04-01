29e0b3cfdd90e358ce5cca3f85d38dd28055ef7b
chiha01 <80226008+chiha01@users.noreply.github.com>
2021-04-01 18:17:31 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,77 +1,274 @@
 #include<SDL/SDL.h>
+
 #include<SDL/SDL_image.h>
+
 #include<SDL/SDL_ttf.h>
+
 #include<stdio.h>
+
+#include <string.h>
+
 #include<stdlib.h>
-#include"enigme.h"
 
-int main ()
+#include"EnigmeAvecFichier.h"
+
+int main()
+
 {
 
+ //initialitation des resources
 
- // initialisation des ressources
 SDL_Surface *screen=NULL;
+
 TTF_Font *police=NULL;
-SDL_color couleurnoire=(0,0,0);
-Enigme O,e;
+
+SDL_color couleurnoir=(0,0,0);
+
+enigme e ;
+
+char *fichier;
+
+char reponse[50];
+
 SDL_Event event;
+
 int done=1;
-while (done) 
+
+
+while(done)
+
 {
-afficherEnigme(enigme e,SDL_Surface *screen); // affichage enigme
-O=Enigme generer(); // generation des questions reponses
-float reponse ;
 
+afficherEnigme(e,*screen); // affichage enigme
 
- while(SDL_PollEvent(&event)) //récuperation des events
-          { 
-             switch(event.type) 
-           { 
-               case SDL_QUIT;
+A=Enigme generer (*e,*fichier); //generation des questions reponses
+
+char reponse1[100];
+
+char reponse2[100];
+
+char reponse3[100];
+
+strcat(ReponseJuste1,reponse1a);
+strcat(ReponseJuste2,reponse2b);
+strcat(ReponseJuste,reponse3c);
+
+while(SDL_PollEvent(&event)) //récuperation des events
+
+          {
+
+             switch(event.type)
+
+           {
+
+               case SDL_QUIT:
+
                   done=0;
-                BREAK; 
 
-                 case SDL_KEYDOWN;
+                BREAK;
+
+                 case SDL_KEYDOWN:
+
                    if(event.key.keysym.sym==SDLK_A)
-                       { 
-                          reponse =1; 
-                        }
-  if(event.key.keysym.sym==SDLK_B)
-                       { 
-                           reponse=2;
-                        }
-  if(event.key.keysym.sym==SDLK_C)
-                       { 
-                          reponse =3; 
+
+                       {
+
+                           strcat (reponse1 , reponse1a);
+
                         }
 
-if (reponse == e.reponseJuste) // la reponse 3 est toujours vrai 
+                 if(event.key.keysym.sym==SDLK_B)
+
+                       {
+
+                           strcat (reponse1 , reponse1b);
+
+                        }
+
+                 if(event.key.keysym.sym==SDLK_C)
+
+                       {
+
+                         strcat (reponse1 , reponse1c);
+
+                        }
+
+
+
+if(event.key.keysym.sym==SDLK_D)
+
+                       {
+
+                           strcat (reponse1 , reponse1d);
+
+                        }
+
+                   if(event.key.keysym.sym==SDLK_E)
+
+                       {
+
+                          strcat (reponse2 , reponse2a);
+
+                        }
+
+                   if(event.key.keysym.sym==SDLK_F)
+
+                       {
+
+                            strcat (reponse2 , reponse2b);
+
+                        }
+
+
+if(event.key.keysym.sym==SDLK_G)
+
+                       {
+
+                            strcat (reponse2 , reponse2c);
+
+                        }
+
+                   if(event.key.keysym.sym==SDLK_H)
+
+                       {
+
+                        strcat (reponse2 , reponse2d);
+
+                        }
+
+                   if(event.key.keysym.sym==SDLK_I)
+
+                       {
+
+                          strcat (reponse3 , reponse3a);
+
+                        }
+
+    if(event.key.keysym.sym==SDLK_J)
+
+                       {
+
+                            strcat (reponse3 , reponse3b);
+
+                        }
+
+   if(event.key.keysym.sym==SDLK_K)
+
+                       {
+
+                            strcat (reponse3 , reponse3c);
+
+                        }
+
+   if(event.key.keysym.sym==SDLK_L)
+
+                       {
+
+                         strcat (reponse3 , reponse3d);
+
+                        }
+
+if (strcmp(reponse1,reponseJuste1)==0)
+
 {
- SDL_BlitSurface(fond,NULL,ecran,&position); //blit du fond
+
+SDL_BlitSurface(fond,NULL,ecran,&position); //blit du fond
+
   //chargement de la police
+
 police=TTF_OpenDont("POLICE.TTF",TAILLE);
 
    //ecriture du texte
-texte=TTF_RenderText_Blended(POLICE,"FÉLICITATION",couleurnoire);
+
+texte=TTF_RenderText_Blended(POLICE,"FÉLICITATION ",couleurnoire);
 
 }
-else // réponse fausse
+
+else 
+
 {
+
  SDL_BlitSurface(fond,NULL,ecran,&position); //blit du fond
+
   //chargement de la police
+
 police=TTF_OpenDont("POLICE.TTF",TAILLE);
 
    //ecriture du texte
+
 texte=TTF_RenderText_Blended(POLICE,"GAME OVER",couleurnoire);
+
 }
+
+if (strcmp(reponse2,reponseJuste2)==0)
+
+{
+
+SDL_BlitSurface(fond,NULL,ecran,&position); //blit du fond
+
+  //chargement de la police
+
+police=TTF_OpenDont("POLICE.TTF",TAILLE);
+
+   //ecriture du texte
+
+texte=TTF_RenderText_Blended(POLICE,"FÉLICITATION ",couleurnoire);
+
+}
+
+else 
+
+{
+
+ SDL_BlitSurface(fond,NULL,ecran,&position); //blit du fond
+
+  //chargement de la police
+
+police=TTF_OpenDont("POLICE.TTF",TAILLE);
+
+   //ecriture du texte
+
+texte=TTF_RenderText_Blended(POLICE,"GAME OVER",couleurnoire);
+
+}
+
+if ( (strcmp(reponse3,reponseJuste3)==0)
+
+{
+
+SDL_BlitSurface(fond,NULL,ecran,&position); //blit du fond
+
+  //chargement de la police
+
+police=TTF_OpenDont("POLICE.TTF",TAILLE);
+
+   //ecriture du texte
+
+texte=TTF_RenderText_Blended(POLICE,"FÉLICITATION vous avez réussi l’enigme",couleurnoire);
+
+}
+
+else 
+
+{
+
+ SDL_BlitSurface(fond,NULL,ecran,&position); //blit du fond
+
+  //chargement de la police
+
+police=TTF_OpenDont("POLICE.TTF",TAILLE);
+
+   //ecriture du texte
+
+texte=TTF_RenderText_Blended(POLICE,"GAME OVER",couleurnoire);
+
+}
+
+
  TTF_CloseFont(police);
+
   TTF_Quit();
+
   SDL_FreeSurface(texte);
-return 0;
+
 }
-
-
-
-
-
-
